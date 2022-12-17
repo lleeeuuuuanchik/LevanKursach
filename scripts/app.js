4a417b9c0a77baeb8ac922ac7074ef070b75e1e5
lleeeuuuuanchik <levanlazarov20@gmail.com>
2022-12-17 20:14:05 +0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,163 @@
+Vue.createApp({
+  data() {
+      return {
+        // switch pages
+        currentPage: 'Registration',
+
+        // slider
+        sliderImgArray: [
+          'SLIDE1.PNG',
+          'SLIDE2.PNG',
+          'SLIDE3.PNG',
+          'SLIDE4.PNG',
+          'SLIDE5.PNG',
+        ],
+        currentSlide: 0,
+
+        //products
+        productsArray: [
+          { name: 'Death Stranding',    img: 'PRODUCT1.PNG', category: "Game-film", price: 99, inf: "THE BEST GAME"},
+          { name: 'Metal Gear Solid V', img: 'PRODUCT2.PNG', category: "Shooter",   price: 89, inf: "THE BEST GAME"},
+          { name: 'Metal Gear Rising',  img: 'PRODUCT3.PNG', category: "Shooter",   price: 79, inf: "THE BEST GAME"},
+          { name: 'Silent Hill',        img: 'PRODUCT4.PNG', category: "Horor",     price: 69, inf: "THE BEST GAME"},
+        ],
+
+          // filters
+        filtersArray: [
+          'name', 'price'
+        ],
+        currentFilter: '',
+
+        categoriesArray: [
+          'Game-film', 'Shooter', 'Horor'
+        ],
+        currentCategory: '',
+        
+        // login and reg
+        ifWrongEmailOrPassword: "",
+        ifWrongRegistration: "",
+        usersArray: [
+          {userName: 'admin', userEmail: 'admin@gmail.com', userPassword: 'admin', userRole: 'admin'},
+        ],
+        currentUser: '',
+
+          // login
+        formLogin: {
+          userEmail: '',
+          userPassword: '',
+        },
+
+          // reg
+        formReg: {
+          userName: '',
+          userPhone: '',
+          userEmail: '',
+          userPassword: '',
+          userRePassword: '',
+          userCheck: false,
+        }
+    }
+  },
+
+  methods: {
+    // slider
+    next() {
+      this.currentSlide + 1 > this.sliderImgArray.length -1 ? this.currentSlide = 0 : this.currentSlide++;
+    },
+
+    // login and reg
+    login() {
+      if (this.formLogin.userEmail && this.formLogin.userPassword) {
+        const user = this.usersArray.find(el => el.userEmail === this.formLogin.userEmail && el.userPassword === this.formLogin.userPassword)
+        if(user) {
+          this.currentUser = user
+          this.currentPage = 'Main'
+          this.formLogin.userEmail = ''
+          this.formLogin.userPassword = ''
+          this.ifWrongEmailOrPassword = ''
+        }
+        else {
+          this.formLogin.userPassword = ''
+          this.ifWrongEmailOrPassword = "Password or email is wrong"
+        }
+      }
+    },
+
+    reg() {
+      if (this.formReg.userPassword == this.formReg.userRePassword) {
+        if (this.formReg.userCheck) {
+          const emailCheck = this.usersArray.find(el => el.email == this.formReg.userEmail)
+          if (emailCheck) {
+            console.log("Такая почта существует")
+            this.ifWrongRegistration = 'This email is already exist in system'
+          }
+          else {
+            const user = {...this.formReg}
+            delete user.userPhone
+            delete user.userRePassword
+            delete user.userCheck
+            user.userRole = 'user'
+            this.usersArray.push(user)
+            this.currentUser = user
+            this.formReg.userName = ""
+            this.formReg.userNumber = ""
+            this.formReg.userEmail = ""
+            this.formReg.userPassword = ""
+            this.formReg.userRePassword = ""
+            this.formReg.userConfirm = false
+            this.currentPage = "Main"
+          }
+        }
+      }
+      else {
+        this.ifWrongRegistration = 'Please, make sure you have confirmed your password correctly.'
+      }
+    }
+  },
+
+  computed: {
+    // slider
+    tSlide() {
+      return `transform: translate(-${this.currentSlide * 100}%)`
+    }, 
+
+    // filters 
+    copyProductsArray() {
+      let copy = [...this.productsArray]
+      if (this.currentFilter) {
+        copy.sort((a, b) => {
+          if (a[this.currentFilter] > b[this.currentFilter]) {
+            return 1
+          }
+          if (a[this.currentFilter] == b[this.currentFilter]) {
+            return 0
+          }
+          if (a[this.currentFilter] < b[this.currentFilter]) {
+            return -1
+          }
+        })
+      }
+      if(this.currentCategory) {
+        return copy.filter( el => el.category == this.currentCategory)
+      }
+      return copy
+    },
+
+    // login and reg
+    isAuth() {
+      return this.currentUser
+    },
+
+    isAdmin() {
+      return this.currentUser && this.currentUser.role == 'admin'
+    }
+  },
+
+  mounted() {
+    // slider
+    setInterval( () => {
+      this.next()
+    }, 1500)
+  }
+
+}).mount('#app')
